0c34f1bcff91adb239186d549271a4d7d7db8c43
Admin <Admin@DESKTOP-6PQE64G>
2024-01-14 19:15:07 +0530

    Add project files.

---
+++ area of square/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace squareapp {
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int s = 5;
+            int area = s * s;
+            Console.WriteLine(area);
+        }
+        
+    }
+}
